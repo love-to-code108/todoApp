f9cab9ee9e84f9b76e5f6fdca28eef6b9d2ed0e0
love.to.code108 <love.to.code108@gmail.com>
2024-03-16 02:02:33 +0530

    the backend is ready for some serious stuff now

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Server Started");
+const app = require('express')();
+
+
+
+
+app.get("/" , (req,res) => {
+    res.send("<h1>Hello world</h1>")
+} )
+
+
+
+app.listen(3000 , () => {
+    console.log("Server started on port 3000");
+})
