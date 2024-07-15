b5e49228e73eb3c12f5c1ca208915f3077cbb66a
love.to.code108 <love.to.code108@gmail.com>
2024-07-15 16:56:39 +0530

    working on the frontend part of the main page

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -14,6 +14,23 @@ import { updateUserObject } from "../axios/useUpdateUserObject"
 // COMPONENTS
 
 
+// ASSETS
+import defaultProfilPic from "../assets/png/profile-user.png"
+import seachIcon_svg from "../assets/svg/search-icon.svg"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const Main = () => {
 
 
@@ -99,21 +116,33 @@ export const Main = () => {
     <div id="main-MainPage">
 
       <div className="wrapper1-MainPage">
-        {/* THE FILES MENU */}
 
-        {/* THE AVATAR LOGO */}
 
-        {/* THE USERNAME */}
 
-        {/* THE EMAIL */}
 
-        {/* THE SEARCH BAR */}
+        {/* THE USERNAME THE EMAIL AND THE LOGO   THE ACCOUNT SECTION */}
+        <div className=" py-4 flex justify-start
+         2xl:h-[7rem] 2xl:mb-4">
 
-        {/* THE SEARCH BUTTON */}
 
-        {/* THE FILE NAMES LIST */}
+          {/* THE AVATAR LOGO */}
+          <div className="">
+            <img className=" 2xl:w-[4.5rem] 2xl:h-[4.5rem] bg-white rounded-full" src={defaultProfilPic} alt="" />
+          </div>
 
-        {/* THE THREE DOTS IN THE LIST */}
+
+
+          <div className=" flex flex-col text-[#000000] 
+          2xl:mt-3 2xl:text-sm 2xl:ml-4 ">
+
+            {/* THE EMAIL */}
+            <p>love.to.code108@gmail.com</p>
+            {/* THE USERNAME */}
+            <p>love-to-code108</p>
+
+          </div>
+
+
         </div>
 
 
@@ -121,20 +150,51 @@ export const Main = () => {
 
 
 
-        <div className="">
-          {/* THE TO DO SECTION */}
 
-          {/* THE TO DO LOGO */}
 
-          {/* THE TO DO INPUT BOX */}
+        {/* THE SEARCH BAR AND THE BUTTON */}
+        <div className="searchBar-mainPage">
 
-          {/* THE TO DO ADD BOX */}
+          {/* THE SEARCH BAR */}
+          <input type="text" className=" text-xl font-inter px-1 
+              2xl:w-[14rem] 2xl:mr-4 2xl:h-[2rem]"/>
 
-          {/* THE COMPLETED SECTION */}
+          {/* THE SEARCH BUTTON  */}
+          <button className="" onClick={printUserObject}>
+            <img src={seachIcon_svg} alt="" />
+            </button>
 
-          {/* THE INCOMPLETED SECTION */}
         </div>
 
+
+        {/* LIST OF FILES */}
       </div>
-      )
+
+
+
+
+
+
+
+
+
+
+
+
+      <div className="">
+        {/* THE TO DO SECTION */}
+
+        {/* THE TO DO LOGO */}
+
+        {/* THE TO DO INPUT BOX */}
+
+        {/* THE TO DO ADD BOX */}
+
+        {/* THE COMPLETED SECTION */}
+
+        {/* THE INCOMPLETED SECTION */}
+      </div>
+
+    </div>
+  )
 }
